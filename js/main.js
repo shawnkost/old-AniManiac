549b89814431dae9cb76de811317afca2cfbd37c
shawnkost <shawnmkost@gmail.com>
2020-12-15 17:19:20 -0800

    Create a row for the top airing anime that includes a heading. #2

--- js/main.js
+++ js/main.js
@@ -1,16 +1,15 @@
-var $imageContainers = document.querySelector('.scrolling-wrapper');
+var $topAnime = document.querySelector('.top-img');
 
 function getTopAnime() {
   var xhr = new XMLHttpRequest();
   xhr.open('GET', 'https://api.jikan.moe/v3/top/anime');
   xhr.responseType = 'json';
   xhr.addEventListener('load', function () {
-    // console.log(xhr.response);
     for (var i = 0; i < xhr.response.top.length; i++) {
       var $img = document.createElement('img');
       $img.setAttribute('src', xhr.response.top[i].image_url);
       $img.setAttribute('alt', xhr.response.top[i].title);
-      $imageContainers.appendChild($img);
+      $topAnime.appendChild($img);
     }
   });
   xhr.send();
